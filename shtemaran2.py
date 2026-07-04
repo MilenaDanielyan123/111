8aa016583d70265ce92015483d4a50ad68bcb322
MilenaDanielyan123 <m.danelyan777@gmail.com>
2026-07-04 11:09:51 +0000

    update

---
+++ shtemaran2.py
@@ -0,0 +1,48 @@
+#n = int(input("please enter 1st number " "")) # Մուտքագրել 2 թիվ և գտնել մեկը մյուսի վրա բաժանելիս ստացված մնացորդը։
+#m = int(input("please enter 2nd number "" "))
+#if n>m:
+#    print(n%m)
+#else:
+#    print(m%n)    
+
+#n = int(input("please enter 1st number " "")) # Մուտքագրել 2 թիվ և գտնել մեկը մյուսի վրա բաժանելիս ստացված քանորդը։
+#m = int(input("please enter 2nd number "" "))
+#if n>m:
+#    print(n//m)
+#else:
+#    print(m//n)    
+
+#n = int(input("please enter a number")) #Մուտքագրել թիվ և տպել դրա վերջին թվանշանը։
+#print(n%10)
+
+#a = float(input("please enter a number"))#Մուտքագրել թիվ և հաշվել այդ թվի քառակուսի արմատը։
+#sqrt = a ** 0.5
+#print(f"the square root of {a} is {sqrt}")
+
+#a = float(input("please enter a number "" "))#Մուտքագրել 2 թիվ և հաշվել 1-ին թվի 2-րդ թվին համարժեք աստիճանը
+#b = float(input("please enter the value of exponent "" "))
+#r = a ** b
+#print(r)
+
+#x = int(input("please enter the value of x")) #Մուտքագրել թիվ x - ի արժեքի համար և հաշվել y-ի արժեքը որոշակի ֆունկցիայի համար։
+#y = 2 * x **3 - 4 *x**2 +17
+#print(f"the corresponding value of y to {x} is {y}")
+
+#x = int(input("Enter the first number: "" ")) #Մուտքագրել 2 թիվ և տպել այդ երկու թվերի գումարի տասնավորը։
+#y = int(input("Enter the second number "" "))
+#print(((x+y)//10)%10)
+
+#x = int(input("Enter the number of chocolates you need to distribute")) #Մուտքագրել քաղցրավենիքի և մարդկանց քնակը, քաղցրավենիքը հավասարապես բաժանել մարդկանց միջև և հասկանալ ավելցուկի քանակը։
+#y = int(input("Enter the number of people "" "))
+#a = x//y
+#b = x - a*y
+#print(f"When we distribute {x} candies between {y} people equally, each person recieves {a} chocolates and remains {b}")
+
+#a = int(input("Enter a number wih 3 digits")) #Մուտքագրել որևէ եռանիշ թիվ և առանձին տպել դրա թվերը։
+#b = a//100
+#c = (a-b*100)//10
+#d = a %10
+#print(b)
+#print(c)
+#print(d)
+
